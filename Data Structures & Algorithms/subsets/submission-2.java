1e261e34bbfc85999e29d6b7d8681d4c63b194dd
Mutahir Kazmi <kazmi.mutahir@gmail.com>
2026-06-12 09:19:01 -0700

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    private Set<List<Integer>> result;
+
+    public List<List<Integer>> subsets(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        this.result = new HashSet<>();
+        compute(stack, nums, 0);
+        return new ArrayList<>(this.result);
+    }
+
+    public void compute(Stack<Integer> stack, int[] nums, int i) {
+        this.result.add(new ArrayList<Integer>(stack));
+        
+        for (;i < nums.length; i++) {
+            stack.push(nums[i]);
+            compute(stack, nums, i+1);
+            stack.pop();                
+        }
+    }
+}
